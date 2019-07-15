7ea86f1040ed3ab3413e0ef3f385ab26198036f5
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-15 09:28:27 +0100

    Add total students + comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#First the code outputs all the student names
 puts 'The students of Villains Academy'
 puts '------------------'
 puts 'Doctor Hannibal Lecter'
@@ -11,3 +12,8 @@ puts 'Freddy Krueger'
 puts 'The Joker'
 puts 'Joffrey Baratheon'
 puts 'Norman Bates'
+#Then we print we print the total number of students
+print "Overall, we have "
+#it’s important that print() doesn’t add new line characters
+print 11
+puts " great students"
