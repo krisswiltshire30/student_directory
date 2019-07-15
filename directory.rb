5bef5aa3ff773d344c4d16792dbcd8916bb97068
krisswiltshire30 <kriss.wiltshire@googlemail.com>
2019-07-15 14:14:39 +0100

    Adds index number at start of output

--- directory.rb
+++ directory.rb
@@ -17,13 +17,31 @@ def print_header
   puts 'The students of Villains Academy'
   puts '------------------'
 end
+# each_with_index used to put number at the beggining of output
 def print(students)
-  students.each { |student| puts "#{student[:name]}, (#{student[:cohort]} cohort)" }
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]}, (#{student[:cohort]} cohort)"
+  end
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} great students"
+    name = gets.chomp
+  end
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
